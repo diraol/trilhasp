9925804ce0a07ae7a0259d3a9b7f826324a96487
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2013-10-27 02:23:33 -0200

    Básico de login e cadastro de usuário adicionado à views, mas ainda não testado

--- trilhasp/modelagem/views.py
+++ trilhasp/modelagem/views.py
@@ -1,1 +1,51 @@
+# coding=utf8
+################################################################################
+#                              Copyright (C) 2013                              #
+#         Diego Rabatone Oliveira - <diraol(at)diraol(dot)eng(dot)br>          #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+# it under the terms of the GNU Affero General Public License as published by  #
+#      the Free Software Foundation, either version 3 of the License, or       #
+#                     (at your option) any later version.                      #
+#                                                                              #
+#       This program is distributed in the hope that it will be useful,        #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#             GNU Affero General Public License for more details.              #
+#                                                                              #
+#  You should have received a copy of the GNU Affero General Public License    #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+################################################################################
 # Create your views here.
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+def do_login(request):
+    errors = []
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request,user)
+        else:
+            errors.append("User is not activated")
+    else:
+        errors.append("User not found or bad password")
+
+    HttpResponseRedirect(request, 'login.html', {'errors': errors})
+
+def logout(request):
+    logout(request)
+    HttpResponseRedirect(request, 'index.html', {})
+
+def createUser(request):
+    user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+    user.save()
+
+
