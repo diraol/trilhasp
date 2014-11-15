8601c10acfc99fb37d21193d0834c465ed67bb98
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-11-15 15:23:40 -0200

    Changing django-admin behaviour on evaluation app

--- trilhasp/evaluation/admin.py
+++ trilhasp/evaluation/admin.py
@@ -1,10 +1,17 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
 # Register your models here.
 from models import *
 
+
+class MyOSMGeoAdmin(admin.OSMGeoAdmin):
+    default_lat = -2701590.32734
+    default_lon = -5192099.80109
+    default_zoom = 12
+    map_width = 800
+
 admin.site.register(EVALQuestion)
-admin.site.register(EVALAnswer)
+admin.site.register(EVALAnswer, MyOSMGeoAdmin)
 admin.site.register(EVALAnswerModel)
 admin.site.register(Buses)
 admin.site.register(BusLine)
