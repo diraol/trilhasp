5953f52f92ce6375546eb5c450f6fb5ef8c28f6d
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-11-15 00:39:31 -0200

    Adding classes to django-admin.

--- trilhasp/evaluation/admin.py
+++ trilhasp/evaluation/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import *
+
+admin.site.register(EVALQuestion)
+admin.site.register(EVALAnswer)
+admin.site.register(EVALAnswerModel)
+admin.site.register(Buses)
+admin.site.register(BusLine)
+admin.site.register(BusCompanies)
--- trilhasp/game/admin.py
+++ trilhasp/game/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import *
+
+admin.site.register(GameCoinModel)
+admin.site.register(GameFinances)
+admin.site.register(GameBusModel)
+admin.site.register(GamePersonalFleet)
