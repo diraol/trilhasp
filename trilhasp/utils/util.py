41b0d9e98b58ca2788630e1f16658c39604cf129
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-12-15 04:18:43 -0200

    Making more permissive

--- trilhasp/utils/util.py
+++ trilhasp/utils/util.py
@@ -5,7 +5,7 @@ import pdb
 class IsStaffOrTargetUser(permissions.BasePermission):
     def has_permission(self, request, view):
         # allow user to list all users if logged in user is staff
-        return view.action == 'retrieve' or request.user.is_staff
+        return request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
         # allow logged in user to view own details, allows staff to view all records
